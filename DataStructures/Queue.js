c936a2581bb35cca12d4137a07aa85d43b982f5a
mharoot <michaelharootoonyan@yahoo.com>
2016-12-24 17:36:48 -0800

    Add files via upload

---
+++ DataStructures/Queue.js
@@ -0,0 +1,13 @@
+/* A Queue object for queue-like functionality over JavaScript arrays. */
+var Queue = function() {
+    this.items = [];
+};
+Queue.prototype.enqueue = function(obj) {
+    this.items.push(obj);
+};
+Queue.prototype.dequeue = function() {
+    return this.items.shift();
+};
+Queue.prototype.isEmpty = function() {
+    return this.items.length === 0;
+};
